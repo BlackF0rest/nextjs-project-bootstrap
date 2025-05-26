df20bd5046944ff13dd9b939c6735ac9d5e52c1d
Ke1S0unD <67108141+BlackF0rest@users.noreply.github.com>
2025-05-26 15:54:33 +0200

    Add car-collector-app/webpack.config.js 1748267672629

---
+++ car-collector-app/webpack.config.js
@@ -0,0 +1,30 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const webpack = require('webpack');
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync({
+    ...env,
+    babel: {
+      dangerouslyAddModulePathsToTranspile: ['@expo/vector-icons']
+    }
+  }, argv);
+
+  // Use browser's native crypto
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    crypto: false
+  };
+
+  // Add plugins
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_DEBUG': false,
+    })
+  );
+
+  return config;
+};
